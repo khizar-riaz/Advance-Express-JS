f7a11a89e350a57b7b42e100fc86ed4a8466d53a
MUHAMMAD KHIZAR RIAZ <58303763+khizar-riaz@users.noreply.github.com>
2023-10-12 12:57:49 +0200

    Rendering the index page with EJS

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const app = express();
 
 const port = 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
 
 app.use(express.static(path.join(__dirname, "./static")));
 
 app.get("/", (request, response) => {
-  response.sendFile(path.join(__dirname, "./static/index.html"));
+ response.render("pages/index", { pageTitle: "welcome" });
 });
 
 app.get("/speakers", (request, response) => {
