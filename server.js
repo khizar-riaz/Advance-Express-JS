b3aaef37dc4721b2a334c8f5f0452b2bbc346320
MUHAMMAD KHIZAR RIAZ <58303763+khizar-riaz@users.noreply.github.com>
2023-10-12 12:47:46 +0200

    Setting up ejs template

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, './static')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.use(express.static(path.join(__dirname, "./static")));
 
 app.get("/", (request, response) => {
-  response.sendFile(path.join(__dirname, './static/index.html'));
+  response.sendFile(path.join(__dirname, "./static/index.html"));
 });
 
 app.get("/speakers", (request, response) => {
-    response.sendFile(path.join(__dirname, './static/speakers.html'));
-  });
+  response.sendFile(path.join(__dirname, "./static/speakers.html"));
+});
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
