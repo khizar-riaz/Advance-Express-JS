7ec58bee05044ad434e6edbc6cdd34428035a329
MUHAMMAD KHIZAR RIAZ <58303763+khizar-riaz@users.noreply.github.com>
2023-10-16 12:51:19 +0200

    Add a session management middleware

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,14 @@ const feedbackRoute = require("./feedback");
 
 const router = express.Router();
 
-module.exports = params => {
+module.exports = (params) => {
   router.get("/", (request, response) => {
+    if (!request.session.visitcount) {
+      request.session.visitcount = 0;
+    }
+    request.session.visitcount += 1;
+    console.log(`number of visit: ${request.session.visitcount}`);
+
     response.render("pages/index", { pageTitle: "welcome" });
   });
 
